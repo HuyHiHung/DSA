93727939bff3be2e5b26d1eeeb2342fe14c4c426
HuyHiHung <126769158+HuyHiHung@users.noreply.github.com>
2026-09-15 14:27:19 +0700

    Time: 0 ms (100%), Space: 28.8 MB (86.05%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int HammingWeight(int n) {
+        int count = 0;
+        while( n > 0)
+            {
+                int bit = n & 1;
+                if(bit == 1) count++;
+                n >>= 1;
+            }
+        return count;
+    }
+}
